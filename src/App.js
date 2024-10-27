d075aad7b48ca9c0c2bf8f8f00ae40dcc27efe51
Ayhan <ayhan.gadjibalayev@gmail.com>
2024-10-27 14:20:51 +0300

    welcome

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
+import glaz from './images/glaz.png';
+import zamok from './images/zamok.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className='h1'><b>Welcome back !</b></span>
+      <span className='start'>Start managing your finance faster and better</span>
+      <div>
+        <img src={logo} className="App-logo2 logo5" alt="logo" />
+        <input className='input3' placeholder='you@exmaple.com' />
+      </div>
+
+      <div>
+        <input className='input2' placeholder='you@exmaple.com' />
+        <div className='www'>
+          <img src={zamok} className="App-logo2 logo3" alt="zamok" />
+          <img src={glaz} className="App-logo2 logo4" alt="glaz" />
+        </div>
+      </div>
+
+      <a href='#' className='password'>forgot password?</a>
+
+      <button className='button'>Login</button>
+
+      <div className='sss'>
+        <p className='start'>Don`t you have an account? </p>
+        <a className='blue' href='#'> Sing Up</a>
+      </div>
+
     </div>
   );
 }
